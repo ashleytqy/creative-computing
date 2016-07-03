139b3729d89cfec8f798ce8099da6c3882a357b9
ashley teoh <ashleytqy@gmail.com>
2016-07-03 09:58:28 -0400

    updated colorsArray, created line method

--- cheetos-paintbrush/sketch.js
+++ cheetos-paintbrush/sketch.js
@@ -1,10 +1,11 @@
 var video;
-var colorArray = [];
 var prevXPos;
 var prevYPos;
 
-// A variable for the color we are searching for.
+//a variable for the color we are searching for.
 var trackColor;
+//arrays to store elements that need to be redrawn every frame
+var colorArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,20 +13,19 @@ function setup() {
   video.size(320 * 1.5, 240 * 1.5); //camera size
   video.hide();
 
-  // Start off tracking for red
+  //start off tracking for red
   trackColor = [255, 0, 0];
 }
 
 function draw() {
 
   background(255);
-  
+
   //for the mirror effect, to make the video look more natural
   translate(width, 0);
   scale(-1.0, 1.0);
   // draw the video to canvas
-  image(video, 0, 0); 
-
+  image(video, 0, 0);
   video.loadPixels();
 
   // Before we begin searching, the "world record" for closest color is set to a high number that is easy for the first pixel to beat.
@@ -39,7 +39,7 @@ function draw() {
     for (var x = 0; x < video.width; x++) {
 
       var loc = (x + y * video.width) * 4;
-      
+
       // The functions red(), green(), and blue() pull out the three color components from a pixel.
       var r1 = video.pixels[loc];
       var g1 = video.pixels[loc + 1];
@@ -50,7 +50,8 @@ function draw() {
       var b2 = trackColor[2];
 
       // Using euclidean distance to compare colors
-      var d = dist(r1, g1, b1, r2, g2, b2); // We are using the dist( ) function to compare the current color with the color we are tracking.
+      // We are using the dist( ) function to compare the current color with the color we are tracking.
+      var d = dist(r1, g1, b1, r2, g2, b2);
 
       // If current color is more similar to tracked color than
       // closest color, save current location and current difference
@@ -67,11 +68,13 @@ function draw() {
     // Draw a circle at the tracked pixel
     // Must be close to previous location
     fill(trackColor);
-    strokeWeight(4.0);
-    stroke(0);
     ellipse(closestX, closestY, 16, 16);
-    var size = map(mouseX, 0, width, 0, 50);
-    colorArray.push(closestX, closestY, size, size);
+
+    var size = 10;
+    //to-do: map 'size' to something more meaningful and not mouseX;
+
+    //add new X and Y to the colorArray
+    colorArray.push(closestX, closestY, size, size, trackColor);
     //update prevXPos and prevYPos
     prevXPos = closestX;
     prevYPos = closestY;
@@ -90,8 +93,22 @@ function mousePressed() {
 }
 
 function paint() {
-  for (var i = 0; i < colorArray.length; i += 4) {
+  for (var i = 0; i < colorArray.length; i += 5) {
     noStroke();
+    //trackColor is stored in colorArray[i+4]
+    fill(colorArray[i + 4]);
+    //draw ellipse at closestX and closestY
     ellipse(colorArray[i], colorArray[i + 1], colorArray[i + 2], colorArray[i + 3]);
+    noFill();
+
+    if (i - 2 <= colorArray.length) {
+      stroke(5);
+      //trackColor is stored in colorArray[i+4]
+      stroke(colorArray[i + 4]);
+      //draw line from prevX and prevY to closestX and closestY
+      line(colorArray[i], colorArray[i + 1], colorArray[i + 5], colorArray[i + 6]);
+      noStroke();
+    }
+
   }
 }
